16c7a45594ed9c8a8d8192023c5d7aaa635e471b
Greg3380 <grzegorz.wator10@gmail.com>
2019-03-18 11:21:30 +0100

    changes in ex 1.1 + output

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,6 +1,6 @@
 import os
+from collections import Counter
 
-import re
 import regex
 
 
@@ -10,6 +10,20 @@ class Date(object):
         self.month = month
         self.year = year
 
+
+class Regulation(object):
+    def __init__(self, title, regulation_year, regulation_number, regulation_poz):
+        self.title = title
+        self.regulation_year = regulation_year
+        self.regulation_number = regulation_number
+        self.regulation_poz = regulation_poz
+
+    def set_title(self, title):
+        self.title = title
+
+
+def build_reference_name(reference):
+    return str(reference.regulation_year) + " position: " +  reference.regulation_poz + " journal number: " + str(reference.regulation_number)
 
 def split(txt, seps):
     if len(seps) == 0:
@@ -70,52 +84,62 @@ def find_positions(string, number):
 
 def match_modern_case(string):
     modern_pattern = r'(?<=(\d\d\d\d)\s*r.*?)(?<=poz)*?(\d+)'  # ten pattern jest ok (positive lookbehind)
-    results = regex.findall(modern_pattern, string, regex.IGNORECASE | regex.MULTILINE | regex.DOTALL)  # TODO case gdy nie ma roku w referencji
+    results = regex.findall(modern_pattern, string,
+                            regex.IGNORECASE | regex.MULTILINE | regex.DOTALL)  # TODO case gdy nie ma roku w referencji
     return [(res[0],) + (None,) + (res[1],) for res in results]
 
 
-def match_number_and_position_in_journal(string, date):
+def match_number_and_position_in_journal(string, date, title):
     if regex.search('nr', string, regex.IGNORECASE):
         references = match_old_fashioned_case(string, date)
     else:
         references = match_modern_case(string)
-
-    return references
+    res = [Regulation('', ref[0], ref[1], ref[2]) for ref in references]
+    if len(res) != 0:
+        res[0].set_title(title)
+    return res
 
 
 def match_date(string):
-    pattern = r'(\d+) (\p{L}+) (\d+)(.*?)'
-    res = regex.findall(pattern, string)
+    pattern = r'(\d+)\s+(\p{L}+)\s+(\d+)'
+    res = regex.findall(pattern, string, regex.IGNORECASE | regex.MULTILINE | regex.DOTALL)
+    if len(res) == 0:
+        return Date(-1, -1, -1)
     return Date(extract_number(res[0][0]), res[0][1], extract_number(res[0][2]))
 
 
 def find_all_external_references(bill):
-    pattern = r'ustawie(.*?)\((.*?\))'
+    pattern = r'ustaw(?:a|y|ie|ę|ą|o||om|ami|ach)((?:.){1,200})(\([^()]*?(?:poz)[^()]*\))'
     bills = regex.findall(pattern, bill, regex.IGNORECASE | regex.MULTILINE | regex.DOTALL)
-
+    result = []
     for bill in bills:
 
         if not regex.findall("poz", bill[1], regex.IGNORECASE | regex.MULTILINE | regex.DOTALL):
             continue
-
         title = bill[0]
         date = match_date(title)
-        references = match_number_and_position_in_journal(bill[1], date)
-        print("Bill title:")
-        print(title)
-        print("Year, number in journal, position")
-        print(references)
+        result += match_number_and_position_in_journal(bill[1], date, title)
+    return result
 
 
-path = os.path.join("..", "ustawy", "1994_195.txt")
-
-with open(path, 'r', encoding="utf8") as fin:
-    find_all_external_references(fin.read())
+path = os.path.join("..", "ustawy", "1995_2.txt")
+# with open(path, 'r', encoding="utf8") as fin:
+#     find_all_external_references(fin.read())
 
 directory_path = os.path.join("..", "ustawy")
-#
-# for root, dirs, files in os.walk(directory_path):
-#     for filename in files:
-#         with open(os.path.join(directory_path, filename), 'r', encoding="utf8") as fin:
-#             print("Bill name: " + filename + "\n\n")
-#             find_all_external_references(fin.read())
+
+for root, dirs, files in os.walk(directory_path):
+    result = []
+    for filename in files:
+        with open(os.path.join(directory_path, filename), 'r', encoding="utf8") as fin:
+            print("Bill name: " + filename + "\n\n")
+            references = find_all_external_references(fin.read())
+            if len(references) != 0:
+                result += references
+
+    reference_dict = Counter()
+    for ref in result:
+        reference_dict[build_reference_name(ref)] += 1
+    with open(os.path.join("..", "output", "exercise1_1"), 'w+', encoding="utf8") as fout:
+        for ref, occurrences in reference_dict.most_common():
+            fout.write(ref + " Count: " + str(occurrences) + "\n")
